8db3f82f5bd1c975e6555bc3ea8a72bae9a46e66
avidness <adavidness@gmail.com>
2024-08-20 14:06:13 -0400

    code to build a dictionary based on a list of existing addresses

---
+++ src/build_addr_dictionary.py
@@ -0,0 +1,21 @@
+from pathlib import Path
+import pandas as pd
+from utils.common import df_to_dictionary, add_missing_spaces
+import time
+
+# load data\
+data_dir = Path(__file__).resolve().parent.parent / 'data'
+df = pd.read_csv(data_dir / 'addr_clean.csv')
+df = df.dropna(subset=['address'])
+
+df['address'] = df['address'].apply(add_missing_spaces)
+
+start_time = time.time()
+word_dict = df_to_dictionary(df, 'address', 15)
+end_time = time.time()
+execution_time = end_time - start_time
+print(f"Execution time: {execution_time} seconds")
+
+import pickle
+with open(data_dir / 'addr_word_dict.pkl', 'wb') as f:
+    pickle.dump(word_dict, f)
